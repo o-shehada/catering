1b9b6ddfe1a69ffaba15af5d30fa56bcc18bd6b6
Omar Muhammad <o.shehada@ard.ly>
2026-06-18 13:57:00 +0200

    fix tables

---
+++ catering/catering/doctype/flight_catering_template/flight_catering_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, ARD and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Flight Catering Template", {
+// 	refresh(frm) {
+
+// 	},
+// });
